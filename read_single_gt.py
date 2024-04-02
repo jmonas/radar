106617fb02a17d23054754c2927ebf59a2c8f982
Jack Monas <jmonas@princeton.edu>
2024-04-02 14:17:10 -0400

    gr

--- read_single_gt.py
+++ read_single_gt.py
@@ -1,7 +1,16 @@
 import pickle
-
+import sys
 # Specify the path to your pickle file
-pickle_file_path = '/projects/FHEIDE/RADDet/train/gt/part_7/004872.pickle'
+# pickle_file_path = '/projects/FHEIDE/RADDet/train/gt/part_7/004872.pickle'
+
+
+# Check if the user has provided the file name as a command-line argument
+if len(sys.argv) < 2:
+    print("Usage: python3 read_single_gt.py <pickle_file_path>")
+    sys.exit(1)
+
+# The first command-line argument after the script name is the path to the pickle file
+pickle_file_path = sys.argv[1]
 
 # Open the pickle file in binary read mode
 try:
